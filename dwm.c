fe109212b10ed4edde938d3b28ac9bd35d600ef4
Dieter Home <eickstaedt@deicon.de>
2019-02-26 21:05:05 +0100

    Apply autostart.patch

--- dwm.c
+++ dwm.c
@@ -193,6 +193,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1381,6 +1382,17 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	if (system("cd ~/.dwm; ./autostart_blocking.sh") == -1) {
+		;
+	}
+	if (system("cd ~/.dwm; ./autostart.sh &") == -1) {
+		;
+	}
+	
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2142,6 +2154,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
